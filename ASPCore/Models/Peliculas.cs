932075a70c4baf351ab00056575886bd65d8dca1
Jose Luis Mena <72637622+MenaA-cpu@users.noreply.github.com>
2020-10-09 21:57:56 -0500

    modelo de peliculas añadido

---
+++ ASPCore/Models/Peliculas.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPCore.Models
+{
+    public class Peliculas
+    {
+        public string ID_1 { get; set; }
+        public DateTime Fecha_subida1 { get; set; }
+        public string Nombre_pelicula1 { get; set; }
+        public string Director1 { get; set; }
+        public string Comentarios1 { get; set; }
+        public string Clasificacion1 { get; set; }
+        public string Duracion1 { get; set; }
+
+        public string ID_2 { get; set; }
+        public DateTime Fecha_subida2 { get; set; }
+        public string Nombre_pelicula2 { get; set; }
+        public string Director2 { get; set; }
+        public string Comentarios2 { get; set; }
+        public string Clasificacion2 { get; set; }
+        public string Duracion2 { get; set; }
+
+        public string ID_3 { get; set; }
+        public DateTime Fecha_subida3 { get; set; }
+        public string Nombre_pelicula3 { get; set; }
+        public string Director3 { get; set; }
+        public string Comentarios3 { get; set; }
+        public string Clasificacion3 { get; set; }
+        public string Duracion3 { get; set; }
+    }
+}
